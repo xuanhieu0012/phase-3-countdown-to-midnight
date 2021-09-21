22f888bf875b1111ca8f156e38ef2f76fe51fe2e
Hieu <xuanhieu0012@gmail.com>
2021-09-21 12:51:03 -0500

    commit tree d680a2de774bd453c55bfcd073d4c022a55962ba

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown i
+    
+    while i>0
+        puts "#{i} SECOND(S)!"
+        i-= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep i
+    sleep(5)
+   countdown(i)
+end
+
+countdown(10)
+
